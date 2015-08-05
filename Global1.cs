5b3b3ebe945294876f64ecdcf7d0458de2b93116
Paul Kowalski <pkowalski@cambridge-intl.com>
2015-08-05 11:39:18 -0400

    Initial Commit

---
+++ Global1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.WebData;
+
+namespace OdeToFood
+{
+    public static class Global1
+    {
+        public static void InitializeDatabaseConnection()
+        {
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", 
+                "UserProfile", "UserId", "UserName", autoCreateTables: true);
+        }
+    }
+}
